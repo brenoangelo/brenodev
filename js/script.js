a4c67427059e2fda962fc8784452ff81424c81ac
brenoangelo <brenoangeloal@gmail.com>
2021-06-07 16:14:59 -0300

    adicionado scroll suave

--- js/script.js
+++ js/script.js
@@ -19,5 +19,21 @@ $(()=>{
         })
     /* */
 
+    /* SCROLL SUAVE */
+
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+                targetOffset = $(id).offset().top;
+                
+        $('html, body').animate({ 
+            scrollTop: targetOffset - 100
+        }, 500);
+    });
+
+
+        
+    /* */
+
 
 })
